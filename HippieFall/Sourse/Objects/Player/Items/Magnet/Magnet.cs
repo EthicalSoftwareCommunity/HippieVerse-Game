98b49183af1fdfe459e0498b12c4a36cd578729d
EthicalSoftwareBot000 <116883998+EthicalSoftwareBot000@users.noreply.github.com>
2022-11-11 15:43:32 +0800

    Reduce the chances of a magnet appearing

--- HippieFall/Sourse/Objects/Player/Items/Magnet/Magnet.cs
+++ HippieFall/Sourse/Objects/Player/Items/Magnet/Magnet.cs
@@ -7,54 +7,54 @@ using HippieFall.Collectables;
 
 namespace HippieFall.Items
 {
-    public class Magnet : Item, IEffectable
-    {
-        private List<Collectable> _collectablesInRange;
-        private Vector3 _directionTo;
-        private float _force;
-        private float _speedForceCorrection;
+	public class Magnet : Item, IEffectable
+	{
+		private List<Collectable> _collectablesInRange;
+		private Vector3 _directionTo;
+		private float _force;
+		private float _speedForceCorrection;
 
-        public override void _Ready()
-        {
-            _collectablesInRange = new List<Collectable>();
-            Visible = false;
-            SetPhysicsProcess(false);
-            SetBlockSignals(false);
-        }
+		public override void _Ready()
+		{
+			_collectablesInRange = new List<Collectable>();
+			Visible = false;
+			SetPhysicsProcess(false);
+			SetBlockSignals(false);
+		}
 
-        public override void _PhysicsProcess(float delta)
-        {
-            foreach (var collectable in _collectablesInRange)
-            {
-                if (collectable != null)
-                {
-                    _directionTo = collectable.GlobalTranslation.DirectionTo(GlobalTranslation);
-                    collectable.GlobalTranslation += delta * _directionTo * (_force+_speedForceCorrection/3);
-                }
-            }
-        }
-        private void OnAreaEntered(Area area)
-        {
-            if (area.Owner is Collectable collectable)
-                _collectablesInRange.Add(collectable);
-        }
+		public override void _PhysicsProcess(float delta)
+		{
+			foreach (var collectable in _collectablesInRange)
+			{
+				if (collectable != null)
+				{
+					_directionTo = collectable.GlobalTranslation.DirectionTo(GlobalTranslation);
+					collectable.GlobalTranslation += delta * _directionTo * (_force+_speedForceCorrection/3);
+				}
+			}
+		}
+		private void OnAreaEntered(Area area)
+		{
+			if (area.Owner is Collectable collectable)
+				_collectablesInRange.Add(collectable);
+		}
 
-        private void OnAreaExited(Area area)
-        {
-            if (area.Owner is Collectable collectable)
-                _collectablesInRange.Remove(collectable);
-        }
+		private void OnAreaExited(Area area)
+		{
+			if (area.Owner is Collectable collectable)
+				_collectablesInRange.Remove(collectable);
+		}
 
-        public void ChangeConfigData(Config config)
-        {
-            MagnetConfig magnetConfig = ((ItemConfig)config).MagnetConfig;
-            
-            _force = magnetConfig.Force;
-            _speedForceCorrection = magnetConfig.SpeedForceCorrection;
-            Visible = magnetConfig.IsMagnetActivated;
-            SetPhysicsProcess(magnetConfig.IsMagnetActivated);
-            SetBlockSignals(magnetConfig.IsMagnetActivated);
-        }
-    }
+		public void ChangeConfigData(Config config)
+		{
+			MagnetConfig magnetConfig = ((ItemConfig)config).MagnetConfig;
+			
+			_force = magnetConfig.Force;
+			_speedForceCorrection = magnetConfig.SpeedForceCorrection;
+			Visible = magnetConfig.IsMagnetActivated;
+			SetPhysicsProcess(magnetConfig.IsMagnetActivated);
+			SetBlockSignals(magnetConfig.IsMagnetActivated);
+		}
+	}
 
 }
