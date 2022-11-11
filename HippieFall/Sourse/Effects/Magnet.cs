0779de1fcec1cfb2154ff097cb2b8ce5cb0f618c
EthicalSoftwareBot000 <116883998+EthicalSoftwareBot000@users.noreply.github.com>
2022-11-11 17:30:16 +0800

    moved the camera closer to the hero

--- HippieFall/Sourse/Effects/Magnet.cs
+++ HippieFall/Sourse/Effects/Magnet.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Global.Data;
 using Godot;
 using Global.Data.EffectSystem;
@@ -7,24 +7,24 @@ using HippieFall.Items;
 
 namespace HippieFall.Tunnels
 {
-    public class Magnet : Effect
-    {
-        private float _force;
-        public Magnet(float force = 10f)
-        {
-            Target = EffectsTarget.Player;
-            _force = force;
-        }
+	public class Magnet : Effect
+	{
+		private float _force;
+		public Magnet(float force = 10f)
+		{
+			Target = EffectsTarget.Player;
+			_force = force;
+		}
 
-        public override Config Apply(Config config)
-        {
-            if (config is PlayerConfig playerConfig)
-            {
-                playerConfig.ItemConfig.MagnetConfig.IsMagnetActivated = true;
-                playerConfig.ItemConfig.MagnetConfig.Force = _force;
-                return playerConfig;
-            }
-            return config;
-        }
-    }
+		public override Config Apply(Config config)
+		{
+			if (config is PlayerConfig playerConfig)
+			{
+				playerConfig.ItemConfig.MagnetConfig.IsMagnetActivated = true;
+				playerConfig.ItemConfig.MagnetConfig.Force = _force;
+				return playerConfig;
+			}
+			return config;
+		}
+	}
 }
