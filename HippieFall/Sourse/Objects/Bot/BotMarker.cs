2be908b2f45e5c36b2cf9aea1c943fc79d5b3e01
EthicalSoftwareBot0 <ethical.software.0@gmail.com>
2023-01-10 23:02:13 +0300

    Add smoothy moving for bot

--- HippieFall/Sourse/Objects/Bot/BotMarker.cs
+++ HippieFall/Sourse/Objects/Bot/BotMarker.cs
@@ -1,18 +1,35 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Timers;
 using Global;
 using HippieFall.Game;
+using Timer = System.Timers.Timer;
 
 public class BotMarker : Spatial
 {
     [Export] private List<NodePath> possibleMarkers;
 
     public Spatial Marker;
+    private Vector3 StartPosition;
     public override void _Ready()
     {
         Marker = GetNode<Spatial>(possibleMarkers[Utilities.GetRandomNumberInt(0, possibleMarkers.Count - 1)]);
         HippieFallUtilities.Game.Bot.BotMarkersController.AddNewMarker(this);
         Visible = false;
+        Marker.Translation += new Vector3(Utilities.GetRandomNumberFloat(0, 0.3f),
+            Utilities.GetRandomNumberFloat(0, 0.3f), Utilities.GetRandomNumberFloat(0, 0.3f));
+        StartPosition = Marker.Translation;
+        Timer timer = new Timer(500);
+        timer.Elapsed += ChangeMarkerPosition;
+        timer.Start();
+
     }
+
+    private void ChangeMarkerPosition(object sender, ElapsedEventArgs e)
+    {
+        Translation = StartPosition + new Vector3(Utilities.GetRandomNumberFloat(-1f, 1f),
+            Utilities.GetRandomNumberFloat(-1f, 1f), Utilities.GetRandomNumberFloat(-1f, 1f));
+    }
+    
 }
